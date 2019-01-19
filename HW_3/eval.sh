d98e1e33d47fbb96931507063eb71c3a6e98cd3d
Vladimir Kondenko <kondenko2011@gmail.com>
2019-01-20 01:24:04 +0300

    Don't visualise the world when evaluating

--- HW_3/eval.sh
+++ HW_3/eval.sh
@@ -1,1 +1,1 @@
-python3 run_car.py -s 800 --seed 13 -f "network_config_agent_0_layers_13_6_1.txt" -e True
+python3 run_car.py -s 800 --seed 3 -f "network_config_agent_0_layers_13_6_1.txt" -e True
--- HW_3/run_car.py
+++ HW_3/run_car.py
@@ -12,22 +12,24 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--steps", type=int)
 parser.add_argument("-f", "--filename", type=str)
 parser.add_argument("-e", "--evaluate", type=bool)
+parser.add_argument("-v", "--visualise", action="store_true")
 parser.add_argument("--seed", type=int)
 args = parser.parse_args()
 
-print(args.steps, args.seed, args.filename, args.evaluate)
+print(args.steps, args.seed, args.filename, args.evaluate, args.visualise)
 
 steps = args.steps
 seed = args.seed if args.seed else None # Use a random map
 np.random.seed(seed)
 random.seed(seed)
 m = generate_map(8, 5, 3, 3)
+visual = args.visualise
 
 if args.filename:
     agent = SimpleCarAgent.from_file(args.filename)
     w = SimpleCarWorld(1, m, SimplePhysics, SimpleCarAgent, timedelta=0.2)
     if args.evaluate:
-        print(w.evaluate_agent(agent, steps))
+        print(w.evaluate_agent(agent, steps, visual))
     else:
         w.set_agents([agent])
         w.run(steps)
